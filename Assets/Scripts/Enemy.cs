ec32cb1d220c738682efd78d338e316944f9d1e3
lynn <rainbowkitties927@gmail.com>
2026-03-13 00:29:41 -0400

    change to display all enemies in visual feedback

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
@@ -6,20 +8,30 @@ public class Enemy : MonoBehaviour
     public TimerManager timerManager; 
     private SpriteRenderer sr;
 
+    private static List<Enemy> allEnemies = new List<Enemy>();
+    private static bool isFlashing = false;
+
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         sr.enabled = false;
+
+        allEnemies.Add(this);
+    }
+
+    void OnDestroy()
+    {
+        allEnemies.Remove(this);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // CHOICE A: Visual Feedback (Show the Sprite)
+            // CHOICE A: Visual Feedback (Show all enemy sprites briefly)
             if (MenuLogic.FeedbackType == "Visual")
             {
-                sr.enabled = true; 
+                TriggerGlobalFlash();
             }
             
             // CHOICE B: Text Feedback (Handled inside TimerManager)
@@ -39,5 +51,38 @@ public class Enemy : MonoBehaviour
                 timerManager.StopTouching();
             }
         }
+    }
+
+    private void TriggerGlobalFlash()
+    {
+        if (!isFlashing)
+        {
+            StartCoroutine(FlashCoroutine());
+        }
+    }
+
+    private IEnumerator FlashCoroutine()
+    {
+        isFlashing = true;
+
+        foreach (var enemy in allEnemies)
+        {
+            if (enemy != null && enemy.sr != null)
+            {
+                enemy.sr.enabled = true;
+            }
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        foreach (var enemy in allEnemies)
+        {
+            if (enemy != null && enemy.sr != null)
+            {
+                enemy.sr.enabled = false;
+            }
+        }
+
+        isFlashing = false;
     }
 }
